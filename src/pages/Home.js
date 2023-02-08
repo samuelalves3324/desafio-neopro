182ac6b6f33a1bf977e80b5015606ea02d6b3229
Samuel Alves <samucahhh@gmail.com>
2023-02-08 13:34:30 -0300

    create remove card buttom and add style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import Slider from '../components/Slider';
 import { changeCardNumbersVisibility } from '../redux/actions';
 
-
 export default function Home({ navigation }) {
   const dispatch = useDispatch();
+  const hidden = useSelector(state => state.changeVisibilityReducer.hidden);
   return (
     <ScrollView style={styles.container}>
       <View>
@@ -16,8 +16,18 @@ export default function Home({ navigation }) {
           </TouchableOpacity>
         </View>
         <Slider />
+        <Text style={styles.actionsTitle}>Ações</Text>
         <TouchableOpacity style={styles.hideButton} onPress={() => dispatch(changeCardNumbersVisibility())}>
-          <Text style={styles.hideButtonTitle}>ESCONDER NÚMERO</Text>
+          {hidden ?
+            <Image style={styles.hideButtonImg} source={require(`../images/hide.png`)} />
+            : <Image style={styles.hideButtonImg} source={require(`../images/view.png`)} />}
+          {hidden ?
+            <Text style={styles.hideButtonTitle}>MOSTRAR NÚMERO</Text>
+            : <Text style={styles.hideButtonTitle}>ESCONDER NÚMERO</Text>}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.removeCardButton}>
+          <Image style={styles.removeCardButtonImg} source={require('../images/bin.png')} />
+          <Text style={styles.removeCardButtonTitle}>APAGAR CARTÃO</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
@@ -50,15 +60,46 @@ const styles = StyleSheet.create({
     margin: 0,
   },
   hideButton: {
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    padding: 20,
-    backgroundColor: 'red',
-    margin: 20,
+    padding: 15,
+    marginHorizontal: 20,
+    marginVertical: 10,
     borderRadius: 10,
+    flexDirection: 'row',
   },
   hideButtonTitle: {
     fontSize: 15,
     fontWeight: 'bold',
+    color: '#2D2940',
   },
+  hideButtonImg: {
+    marginHorizontal: 20,
+    width: 30,
+    height: 30,
+  },
+  removeCardButton: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 15,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+  },
+  removeCardButtonTitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#F21D1D',
+  },
+  removeCardButtonImg: {
+    marginHorizontal: 22.5,
+    width: 25,
+    height: 25,
+  },
+  actionsTitle: {
+    margin: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
